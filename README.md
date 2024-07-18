1a9b97641d6070ef7b85346851c62237241f26fb
tonyking <tony@king.com>
2024-07-18 19:58:48 +1000

    Upload to backend

--- README.md
+++ README.md
@@ -1,1 +1,67 @@
-# speech-demo-stt-fe-indirect
+# React Speech service sample app
+
+This sample shows how to integrate the backend for speech-to-text conversions.
+
+## How to run the app
+
+1. Clone this repo, then change directory to the project root and run `npm install` to install dependencies.
+1. To run the Express server and React app together, run `npm start`.
+
+## Speech-to-text from microphone
+
+This sample updates the solution of previous lab. Use react-mic to record audio and send the file to the backend to handle.  
+
+```javascript
+const onStop = async (recordedBlob) => {
+  console.log('Recorded Blob: ', recordedBlob);
+  if (recordedBlob && recordedBlob.blob) {
+    const formData = new FormData();
+    formData.append('file', recordedBlob.blob, 'recording.wav');
+
+    try {
+      const response = await axios.post('http://localhost:5001/api/Voice/upload', formData, {
+        headers: {
+          'Content-Type': 'multipart/form-data'
+        }
+      });
+      var text = response.data.DisplayText || response.data.displayText || 'No transcription available'
+      console.log("text is " + text)
+      setDisplayText(text);
+      console.log('Transcription: ', response.data);
+    } catch (error) {
+      console.error('Error uploading the file:', error);
+      setDisplayText('Error uploading the file.');      }
+  } else {
+    console.error('Recorded blob is empty or invalid');
+  }
+};
+```
+
+## Speech-to-text from file
+
+Update the handleFileChange to following based on the previous lab.  
+
+```javascript
+const handleFileChange = async (event) => {
+
+  const audioFile = event.target.files[0];
+
+  const formData = new FormData();
+  formData.append('file', audioFile);
+
+  try {
+    const response = await axios.post('http://localhost:5001/api/Voice/upload', formData, {
+      headers: {
+        'Content-Type': 'multipart/form-data'
+      }
+    });
+
+    setDisplayText(response.data.DisplayText || response.data.displayText || 'No transcription available');
+  } catch (error) {
+    console.error('Error uploading the file:', error);
+  }
+};
+```
+
+You need the audio file as a JavaScript [`File`](https://developer.mozilla.org/en-US/docs/Web/API/File) object, so you can grab it directly off the event target using `const audioFile = event.target.files[0];`. Next, you use the file to create the `AudioConfig` and then pass it to the recognizer.
+
---
+++ src/App.js
@@ -0,0 +1,162 @@
+import React, { useState } from 'react';
+import { Container } from 'reactstrap';
+import { getTokenOrRefresh } from './token_util';
+import './custom.css'
+import axios from "axios";
+import { ReactMic } from 'react-mic';
+
+const speechsdk = require('microsoft-cognitiveservices-speech-sdk')
+
+export default function App() { 
+    const [displayText, setDisplayText] = useState('INITIALIZED: ready to test speech...');
+    const [player, updatePlayer] = useState({p: undefined, muted: false});
+    const [recording, setRecording] = useState(false);
+
+    const startRecording = () => {
+        setRecording(true);
+    };
+
+    const stopRecording = () => {
+        setRecording(false);
+    };
+
+    const onStop = async (recordedBlob) => {
+        console.log('Recorded Blob: ', recordedBlob);
+        if (recordedBlob && recordedBlob.blob) {
+            const formData = new FormData();
+            formData.append('file', recordedBlob.blob, 'recording.wav');
+
+            try {
+                const response = await axios.post('http://localhost:5001/api/Voice/upload', formData, {
+                    headers: {
+                        'Content-Type': 'multipart/form-data'
+                    }
+                });
+                var text = response.data.DisplayText || response.data.displayText || 'No transcription available'
+                console.log("text is " + text)
+                setDisplayText(text);
+                console.log('Transcription: ', response.data);
+            } catch (error) {
+                console.error('Error uploading the file:', error);
+                setDisplayText('Error uploading the file.');      }
+        } else {
+            console.error('Recorded blob is empty or invalid');
+        }
+    };
+
+    const handleFileChange = async (event) => {
+
+        const audioFile = event.target.files[0];
+
+        const formData = new FormData();
+        formData.append('file', audioFile);
+
+        try {
+            const response = await axios.post('http://localhost:5001/api/Voice/upload', formData, {
+                headers: {
+                    'Content-Type': 'multipart/form-data'
+                }
+            });
+
+            setDisplayText(response.data.DisplayText || response.data.displayText || 'No transcription available');
+        } catch (error) {
+            console.error('Error uploading the file:', error);
+        }
+    };
+
+    async function textToSpeech() {
+        const tokenObj = await getTokenOrRefresh();
+        const speechConfig = speechsdk.SpeechConfig.fromAuthorizationToken(tokenObj.authToken, tokenObj.region);
+        const myPlayer = new speechsdk.SpeakerAudioDestination();
+        updatePlayer(p => {p.p = myPlayer; return p;});
+        const audioConfig = speechsdk.AudioConfig.fromSpeakerOutput(player.p);
+
+        let synthesizer = new speechsdk.SpeechSynthesizer(speechConfig, audioConfig);
+
+        const textToSpeak = displayText.toLowerCase();
+        setDisplayText(`speaking text: ${textToSpeak}...`);
+        synthesizer.speakTextAsync(
+        textToSpeak,
+        result => {
+            let text;
+            if (result.reason === speechsdk.ResultReason.SynthesizingAudioCompleted) {
+                text = `synthesis finished for "${textToSpeak}".\n`
+            } else if (result.reason === speechsdk.ResultReason.Canceled) {
+                text = `synthesis failed. Error detail: ${result.errorDetails}.\n`
+            }
+            synthesizer.close();
+            synthesizer = undefined;
+            setDisplayText(text);
+        },
+        function (err) {
+            setDisplayText(`Error: ${err}.\n`);
+
+            synthesizer.close();
+            synthesizer = undefined;
+        });
+    }
+
+    async function handleMute() {
+        updatePlayer(p => { 
+            if (!p.muted) {
+                p.p.pause();
+                return {p: p.p, muted: true}; 
+            } else {
+                p.p.resume();
+                return {p: p.p, muted: false}; 
+            }
+        });
+    }
+
+    return (
+        <Container className="app-container">
+            <h1 className="display-4 mb-3">Speech sample app</h1>
+
+            <div className="row main-container">
+                <div className="col-6">
+                    <div className="col-6">
+                        <ReactMic
+                          record={recording}
+                          className="sound-wave"
+                          onStop={onStop}
+                          mimeType="audio/webm"
+                          strokeColor="#FF0000"
+                          backgroundColor="#000000"
+                        />
+                    </div>
+                    <div className="mt-2">
+                        <i className="fas fa-microphone fa-lg mr-2" onClick={() => startRecording()}/>
+                        Start recording.
+
+                        <i className="fas fa-microphone-slash fa-lg mr-2" onClick={() => stopRecording()}/>
+                        Stop recording
+                    </div>
+
+                    <div className="mt-2">
+                        <label htmlFor="audio-file"><i className="fas fa-file-audio fa-lg mr-2"/></label>
+                        <input 
+                            type="file" 
+                            id="audio-file" 
+                            onChange={(e) => handleFileChange(e)}
+                            style={{display: "none"}} 
+                        />
+                        Convert speech to text from an audio file.
+                    </div>
+                    <div className="mt-2">
+                        <i className="fas fa-volume-up fa-lg mr-2" onClick={() => textToSpeech()}/>
+                        Convert text to speech.
+                    </div>
+                    <div className="mt-2">
+                        <i className="fas fa-volume-mute fa-lg mr-2" onClick={() => handleMute()}/>
+                        Pause/resume text to speech output.
+                    </div>
+
+                </div>
+                    <textarea className="col-6 output-display rounded"
+                      value={displayText}
+                      onChange={(e) => setDisplayText(e.target.value)}
+                    />
+            </div>
+        </Container>
+    );
+}
